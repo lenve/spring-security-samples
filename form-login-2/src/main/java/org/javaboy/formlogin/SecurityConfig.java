288c8bf9ce01a979185ae69e05cd72a5929934cc
江南一点雨 <wangsong0210@gmail.com>
2020-03-30 20:51:06 +0800

    m

---
+++ form-login-2/src/main/java/org/javaboy/formlogin/SecurityConfig.java
@@ -0,0 +1,66 @@
+package org.javaboy.formlogin;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+/**
+ * @作者 江南一点雨
+ * @公众号 江南一点雨
+ * @微信号 a_java_boy
+ * @GitHub https://github.com/lenve
+ * @博客 http://wangsong.blog.csdn.net
+ * @网站 http://www.javaboy.org
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("javaboy")
+                .password("123").roles("admin");
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/js/**", "/css/**","/images/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login.html")
+                .loginProcessingUrl("/doLogin")
+                .usernameParameter("name")
+                .passwordParameter("passwd")
+                .defaultSuccessUrl("/index")
+                .successForwardUrl("/index")
+                .failureForwardUrl("/f2")
+                .failureUrl("/f1")
+                .permitAll()
+                .and()
+                .logout()
+//                .logoutUrl("/logout")
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout","POST"))
+                .logoutSuccessUrl("/index")
+                .deleteCookies()
+                .clearAuthentication(true)
+                .invalidateHttpSession(true)
+                .permitAll()
+                .and()
+                .csrf().disable();
+    }
+}
