bfae8069e1d06686022b7476f4da8bb4e0157f95
江南一点雨 <wangsong0210@gmail.com>
2020-05-02 21:48:56 +0800

    m

---
+++ verifycode/src/main/java/org/javaboy/formlogin/config/MyAuthenticationProvider.java
@@ -0,0 +1,34 @@
+package org.javaboy.formlogin.config;
+
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @作者 江南一点雨
+ * @微信公众号 江南一点雨
+ * @网站 http://www.itboyhub.com
+ * @国际站 http://www.javaboy.org
+ * @微信 a_java_boy
+ * @GitHub https://github.com/lenve
+ * @Gitee https://gitee.com/lenve
+ */
+public class MyAuthenticationProvider extends DaoAuthenticationProvider {
+
+    @Override
+    protected void additionalAuthenticationChecks(UserDetails userDetails, UsernamePasswordAuthenticationToken authentication) throws AuthenticationException {
+        HttpServletRequest req = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        String code = req.getParameter("code");
+        String verify_code = (String) req.getSession().getAttribute("verify_code");
+        if (code == null || verify_code == null || !code.equals(verify_code)) {
+            throw new AuthenticationServiceException("验证码错误");
+        }
+        super.additionalAuthenticationChecks(userDetails, authentication);
+    }
+}
