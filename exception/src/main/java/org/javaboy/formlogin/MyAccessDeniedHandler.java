6194f659baa40dbbbbdba3ee0a09e2144219d19d
江南一点雨 <wangsong0210@gmail.com>
2020-06-15 13:23:37 +0800

    m

---
+++ exception/src/main/java/org/javaboy/formlogin/MyAccessDeniedHandler.java
@@ -0,0 +1,28 @@
+package org.javaboy.formlogin;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author 江南一点雨
+ * @微信公众号 江南一点雨
+ * @网站 http://www.itboyhub.com
+ * @国际站 http://www.javaboy.org
+ * @微信 a_java_boy
+ * @GitHub https://github.com/lenve
+ * @Gitee https://gitee.com/lenve
+ */
+@Component
+public class MyAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        response.setStatus(403);
+        response.getWriter().write("Forbidden:" + accessDeniedException.getMessage());
+    }
+}
