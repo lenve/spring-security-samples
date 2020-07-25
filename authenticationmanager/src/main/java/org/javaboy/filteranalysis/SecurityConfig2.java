1b4b796bbd0030946a247a4c2fcbb4775e7c3915
wangsong <wangsong0210@gmail.com>
2020-07-25 11:46:45 +0800

    m

--- authenticationmanager/src/main/java/org/javaboy/filteranalysis/SecurityConfig2.java
+++ authenticationmanager/src/main/java/org/javaboy/filteranalysis/SecurityConfig2.java
@@ -18,60 +18,60 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
  * @GitHub https://github.com/lenve
  * @Gitee https://gitee.com/lenve
  */
-//@Configuration
-//public class SecurityConfig2 {
-//    @Bean
-//    UserDetailsService us() {
-//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-//        manager.createUser(User.withUsername("sang").password("{noop}123").roles("admin").build());
-//        return manager;
-//    }
-//
-//    @Configuration
-//    @Order(1)
-//    static class DefaultWebSecurityConfig extends WebSecurityConfigurerAdapter {
-//        UserDetailsService us1() {
-//            InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-//            manager.createUser(User.withUsername("javaboy").password("{noop}123").roles("admin", "aaa", "bbb").build());
-//            return manager;
-//        }
-//
-//        @Override
-//        protected void configure(HttpSecurity http) throws Exception {
-//            http.antMatcher("/foo/**")
-//                    .authorizeRequests()
-//                    .anyRequest().hasRole("admin")
-//                    .and()
-//                    .formLogin()
-//                    .loginProcessingUrl("/foo/login")
-//                    .permitAll()
-//                    .and()
-//                    .userDetailsService(us1())
-//                    .csrf().disable();
-//        }
-//    }
-//
-//    @Configuration
-//    @Order(2)
-//    static class DefaultWebSecurityConfig2 extends WebSecurityConfigurerAdapter {
-//        UserDetailsService us2() {
-//            InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-//            manager.createUser(User.withUsername("江南一点雨").password("{noop}123").roles("user", "aaa", "bbb").build());
-//            return manager;
-//        }
-//
-//        @Override
-//        protected void configure(HttpSecurity http) throws Exception {
-//            http.antMatcher("/bar/**")
-//                    .authorizeRequests()
-//                    .anyRequest().hasRole("user")
-//                    .and()
-//                    .formLogin()
-//                    .loginProcessingUrl("/bar/login")
-//                    .permitAll()
-//                    .and()
-//                    .csrf().disable()
-//                    .userDetailsService(us2());
-//        }
-//    }
-//}
+@Configuration
+public class SecurityConfig2 {
+    @Bean
+    UserDetailsService us() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("sang").password("{noop}123").roles("admin").build());
+        return manager;
+    }
+
+    @Configuration
+    @Order(1)
+    static class DefaultWebSecurityConfig extends WebSecurityConfigurerAdapter {
+        UserDetailsService us1() {
+            InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+            manager.createUser(User.withUsername("javaboy").password("{noop}123").roles("admin", "aaa", "bbb").build());
+            return manager;
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.antMatcher("/foo/**")
+                    .authorizeRequests()
+                    .anyRequest().hasRole("admin")
+                    .and()
+                    .formLogin()
+                    .loginProcessingUrl("/foo/login")
+                    .permitAll()
+                    .and()
+                    .userDetailsService(us1())
+                    .csrf().disable();
+        }
+    }
+
+    @Configuration
+    @Order(2)
+    static class DefaultWebSecurityConfig2 extends WebSecurityConfigurerAdapter {
+        UserDetailsService us2() {
+            InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+            manager.createUser(User.withUsername("江南一点雨").password("{noop}123").roles("user", "aaa", "bbb").build());
+            return manager;
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.antMatcher("/bar/**")
+                    .authorizeRequests()
+                    .anyRequest().hasRole("user")
+                    .and()
+                    .formLogin()
+                    .loginProcessingUrl("/bar/login")
+                    .permitAll()
+                    .and()
+                    .csrf().disable()
+                    .userDetailsService(us2());
+        }
+    }
+}
